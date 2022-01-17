1ee0f1bc524d13ddca8f56add292aa188b846de6
ChilledCosmonaut <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-01-17 13:56:05 +0100

    Added basic libraries to project

---
+++ code/game/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
