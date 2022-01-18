d7ccc8296278c63632213ebbeb4e53a49f36c1d5
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-01-18 15:55:49 +0100

    Added shaders temporary in main so that develop has working window

--- code/game/src/main.cpp
+++ code/game/src/main.cpp
@@ -7,17 +7,37 @@
 
 double deltaTime;
 
-float zRotation = 0.0f;
-float rotSpeed  = 120.0f;
-
 const float W_WIDTH = 800.0f;
 const float W_HEIGHT = 600.0f;
 const char* W_TITLE = "GameLab III";
 
+float zRotation = 0.0f;
+float rotStep = 90.0f;
+float xTranslate = 0.0f, yTranslate = 0.0f;
+float transStep = 1.0f;
 
-void modelTransform(gl3::shader* shaderProgram);
-void viewTransform(gl3::shader* shaderProgram);
-void projectionTransform(gl3::shader* shaderProgram);
+const char* vShaderSource = "#version 460 core\n"
+                            "layout (location = 0) in vec3 aPos;\n"
+                            "uniform mat4 model;\n"
+                            "uniform mat4 view;\n"
+                            "uniform mat4 projection;\n"
+                            "void main()\n"
+                            "{\n"
+                            "    gl_Position = projection * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+                            "}\n";
+
+const char* fShaderSource = "#version 460 core\n"
+                            "out vec4 fragColor;\n"
+                            "void main()\n"
+                            "{\n"
+                            "   fragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+                            "}\n";
+
+
+void modelTransform(unsigned int shaderProgram);//gl3::shader* shaderProgram);
+void viewTransform(unsigned int shaderProgram);//gl3::shader* shaderProgram);
+void projectionTransform(unsigned int shaderProgram);//gl3::shader* shaderProgram);
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -29,16 +49,22 @@ void processUserInput(GLFWwindow *window){
         glfwSetWindowShouldClose(window, true);
 
     // user input
-    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
-        std::cout << "zRotation: " << zRotation << std::endl;
+    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
+        zRotation -= rotStep * deltaTime;
     }
 
-    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
-        std::cout << "zRotation: " << zRotation << std::endl;
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
+        zRotation += rotStep * deltaTime;
     }
 
     if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
-        // add your own input here
+        yTranslate += sin(glm::radians(zRotation)) * transStep * deltaTime;
+        xTranslate += cos(glm::radians(zRotation)) * transStep * deltaTime;
+    }
+
+    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
+        yTranslate -= sin(glm::radians(zRotation)) * transStep * deltaTime;
+        xTranslate -= cos(glm::radians(zRotation)) * transStep * deltaTime;
     }
 }
 
@@ -50,7 +76,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(W_WIDTH, W_HEIGHT, "GameLabIII", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GameLabIII", nullptr, nullptr);
     if (window == nullptr)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -67,8 +93,50 @@ int main() {
         return -1;
     }
 
-    gl3::shader shaderProgram = gl3::shader("shaders/vertexShader.glsl","shaders/fragmentShader.glsl");
-    shaderProgram.use();
+    //Vertex Shader
+    unsigned int vShader;
+    vShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vShader, 1, &vShaderSource, nullptr);
+    glCompileShader(vShader);
+
+    int success;
+    char infoLog[512];
+
+    glGetShaderiv(vShader, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(vShader, 512, nullptr, infoLog);
+        std::cout<<"ERROR::SHADER::VERTEX::COMPILATION_FAILED" << infoLog << std::endl;
+    }
+
+    // Fragment Shader
+    unsigned int fShader;
+    fShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fShader, 1, &fShaderSource, nullptr);
+    glCompileShader(fShader);
+
+    glGetShaderiv(fShader, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(fShader, 512, nullptr, infoLog);
+        std::cout<<"ERROR::SHADER::FRAGMENT::COMPILATION_FAILED" << infoLog << std::endl;
+    }
+
+    // SHader Program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vShader);
+    glAttachShader(shaderProgram, fShader);
+    glLinkProgram(shaderProgram);
+
+    glGetShaderiv(shaderProgram, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cout<<"ERROR::SHADER::PROGRAM::LINKING_FAILED" << infoLog << std::endl;
+    }
+
+    glUseProgram(shaderProgram);
+
+    /*gl3::shader shaderProgram = gl3::shader("shaders/vertexShader.glsl","shaders/fragmentShader.glsl");
+    shaderProgram.use();*/
 
     // Space Ship vertices
     float vertices[] = {
@@ -100,14 +168,16 @@ int main() {
 
     while(!glfwWindowShouldClose(window))
     {
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         processUserInput(window);
 
-        modelTransform(&shaderProgram);
-        viewTransform(&shaderProgram);
-        projectionTransform(&shaderProgram);
+        modelTransform(shaderProgram);
+        viewTransform(shaderProgram);
+        projectionTransform(shaderProgram);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         // update deltaTime
         deltaTime = glfwGetTime();
@@ -121,30 +191,41 @@ int main() {
     return 0;
 }
 
-void modelTransform(gl3::shader* shaderProgram){
+void modelTransform(unsigned int shaderProgram){//gl3::shader* shaderProgram){
 
     glm::mat4 model = glm::mat4(1.0f);
-    model = glm::translate(model, glm::vec3(0, 0, 0.0f));
+    model = glm::translate(model, glm::vec3(xTranslate, yTranslate, 0.0f));
     model = glm::rotate(model, glm::radians(zRotation), glm::vec3(0.0f, 0.0f, 1.0f));
     model = glm::scale(model, glm::vec3(0.25f, 0.25f, 0.25f));
 
-    shaderProgram->setMatrix("model", model);
+    //shaderProgram->setMatrix("model", model);
+
+    int modelLocation = glGetUniformLocation(shaderProgram, "model");
+    glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
 }
 
-void viewTransform(gl3::shader* shaderProgram){
+void viewTransform(unsigned int shaderProgram){//gl3::shader* shaderProgram){
 
     glm::mat4 view;
-    view = glm::lookAt(glm::vec3(0.0f,0.0f,0.0f),
+    view = glm::lookAt(glm::vec3(0.0f,0.0f,25.0f),
+                       glm::vec3(0.0f,0.0f,0.0f),
+                       glm::vec3(0.0f,1.0f,0.0f));/*glm::vec3(0.0f,0.0f,0.0f),
                        glm::vec3(0.0f,0.0f,-1.0f),
-                       glm::vec3(0.0f,1.0f,0.0f));
+                       glm::vec3(0.0f,1.0f,0.0f));*/
 
-    shaderProgram->setMatrix("view",view);
+    //shaderProgram->setMatrix("view",view);
+
+    int viewLocation = glGetUniformLocation(shaderProgram, "view");
+    glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
 }
 
-void projectionTransform(gl3::shader* shaderProgram){
+void projectionTransform(unsigned int shaderProgram){//gl3::shader* shaderProgram){
 
-    glm::mat4 projection = glm::perspective(60.0f, W_WIDTH/W_HEIGHT, 0.1f, 100.0f);
+    glm::mat4 projection = glm::perspective(glm::radians(15.0f), W_WIDTH/W_HEIGHT, 0.1f, 100.0f);
 
-    shaderProgram->setMatrix("projection", projection);
+    //shaderProgram->setMatrix("projection", projection);
+
+    int projectionLocation = glGetUniformLocation(shaderProgram, "projection");
+    glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(projection));
 }
 
