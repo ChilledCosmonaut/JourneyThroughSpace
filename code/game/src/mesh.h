58ea70700939d6e3035ca6225e81817d58ffea45
ChilledCosmonaut <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-01-26 11:50:12 +0100

    Setup Logic for mesh class

--- code/game/src/mesh.h
+++ code/game/src/mesh.h
@@ -1,6 +1,37 @@
 #pragma once
 
+#include "shader.h"
 
-class mesh {
+#include <glm/vec3.hpp>
+#include <glm/vec2.hpp>
 
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    string type;
+};
+
+class Mesh {
+    public:
+        // mesh data
+        vector<Vertex>       vertices;
+        vector<unsigned int> indices;
+        vector<Texture>      textures;
+
+        Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures);
+        void Draw(gl3::shader &shader);
+    private:
+        //  render data
+        unsigned int VAO, VBO, EBO;
+
+        void setupMesh();
 };
