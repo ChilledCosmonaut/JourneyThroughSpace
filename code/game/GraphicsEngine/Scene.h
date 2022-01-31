49de20c711098b4d8b9e2b45207adb6c67087090
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-01-31 11:05:00 +0100

    Implemented basic functions prototypes

--- code/game/GraphicsEngine/Scene.h
+++ code/game/GraphicsEngine/Scene.h
@@ -1,12 +1,19 @@
 #pragma once
 
+#include "Model.h"
+
 namespace Graphics{
 
     class Scene {
     public:
+        void Render();
 
+    private:
+        void DisplaySkybox();
+        void DisplayLights();
+        void DisplayModels();
 
-
+        vector<std::pair<Model,gl3::shader>> sceneModels;
     };
 }
 
