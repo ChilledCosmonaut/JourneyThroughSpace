5a864f09767759b82a3f0757eda91e7b4a1f41d3
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-02-14 17:57:39 +0100

    Filled functions with proper logic. Transform now supports rotations, translation and scaling via a mix of quaternions and matrices

--- code/game/GraphicsEngine/Transform.cpp
+++ code/game/GraphicsEngine/Transform.cpp
@@ -1,27 +1,59 @@
+#include <iostream>
 #include "Transform.h"
 
 namespace Graphics {
 
-    Transform::Transform() {
-
+    Transform::Transform(glm::vec3 startRotation, glm::vec3 startTranslation, glm::vec3 startScale) {
+        translation = startTranslation;
+        scale = startScale;
+        Rotate(startRotation);
     }
 
     void Transform::Rotate(glm::vec3 targetRotation) {
-        rotation = targetRotation;
+        /*auto rotationVector = glm::vec3(0.0f);
+        rotationVector.x = 1;
+        rotationVector.y = targetRotation.y/targetRotation.x;
+        rotationVector.z = targetRotation.z/targetRotation.x;
+        rotationVector = glm::normalize(rotationVector);
+        rotation = glm::quat(cos(glm::radians(targetRotation.x/2)),
+                             rotationVector.x * sin(glm::radians(targetRotation.x/2)),
+                             rotationVector.x * sin(glm::radians(targetRotation.x/2)),
+                             rotationVector.x * sin(glm::radians(targetRotation.x/2)));*/
+        /*rotationAxis = rotationVector;
+        rotationAngle = rotationVector.x;*/
+        rotation = glm::radians(targetRotation);
+        recalculateModel();
+
     }
 
     void Transform::Translate(glm::vec3 targetTranslation) {
         translation = targetTranslation;
+        recalculateModel();
     }
 
     void Transform::Scale(glm::vec3 targetScale) {
         scale = targetScale;
+        recalculateModel();
     }
 
-    glm::mat4 Transform::GetModelMatrix() {
-        glm::mat4 currentModel = glm::mat4_cast(rotation);
-        currentModel = glm::translate(currentModel,translation);
-        currentModel = glm::scale(currentModel, scale);
-        return currentModel;
+    glm::mat4* Transform::GetModelMatrix() {
+        return &modelMatrix;
+    }
+
+    glm::mat4 Transform::GetInverseModelMatrix() {
+        return inverseModelMatrix;
+    }
+
+    void Transform::recalculateModel() {
+        /*glm::mat4 test = glm::mat4 (1.0f);
+        test = glm::translate(test, translation);
+        test = glm::rotate(test, rotationAngle, rotationAxis);
+        test = glm::scale(test, scale);
+        modelMatrix = test;*/
+        glm::mat4 translateModel = glm::translate(glm::mat4(1.0f), translation);
+        glm::mat4 rotateModel = glm::mat4_cast(glm::quat(rotation));
+        glm::mat4 scaleModel = glm::scale(glm::mat4(1.0f), scale);
+        modelMatrix = translateModel * rotateModel * scaleModel;
+        inverseModelMatrix = scaleModel * rotateModel * translateModel;
     }
 }
--- code/game/GraphicsEngine/Transform.h
+++ code/game/GraphicsEngine/Transform.h
@@ -7,17 +7,27 @@ namespace Graphics {
 
     class Transform {
     public:
-        Transform();
+        explicit Transform(glm::vec3 startRotation = glm::vec3(0.0f,0.0f,0.0f),
+                           glm::vec3 startTranslation = glm::vec3(0.0f,0.0f,0.0f),
+                           glm::vec3 startScale = glm::vec3(1.0f,1.0f,1.0f));
 
         void Rotate(glm::vec3 targetRotation);
         void Translate(glm::vec3 targetTranslation);
         void Scale(glm::vec3 targetScale);
 
-        glm::mat4 GetModelMatrix();
+        glm::mat4* GetModelMatrix();
+        glm::mat4 GetInverseModelMatrix();
 
     private:
-        glm::quat rotation;
-        glm::vec3 translation;
-        glm::vec3 scale;
+        //glm::quat rotation{};
+        glm::vec3 rotation{};
+        //float rotationAngle = 0.0f;
+        glm::vec3 translation{};
+        glm::vec3 scale{};
+
+        glm::mat4 modelMatrix{};
+        glm::mat4 inverseModelMatrix{};
+
+        void recalculateModel();
     };
 }
